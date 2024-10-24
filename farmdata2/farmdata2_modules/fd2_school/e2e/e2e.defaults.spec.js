97dbdac2cc0d163c69b6d63410b5dc2d757ef10e
hemanthkapa <kapa4902@gmail.com>
2024-10-23 22:06:14 -0400

    Improve farm data testing with multiple text verification approaches

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -1,49 +1,35 @@
-describe("Test the harvest report default values", () => {
+describe("Test the harvest report generation", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/e2e")
     })
-      
-    it("Check the page header", () => {
-        cy.get("[data-cy=page-header]")
-            .should("have.text","Harvest Report")
+
+    it("Verify report generation functionality", () => {
+        // Check that report header doesn't exist before generating
+        cy.get("[data-cy=report-header]")
+            .should("not.exist")
+        // Get and click the Generate Report button
+        cy.get("[data-cy=generate-report-button]")
+            .should("exist")
+            .click()
+        cy.get("[data-cy=report-header]")
+            .should("be.visible")
     })
 
-    it("Check the default start and end dates", () => {
-        cy.get("[data-cy=start-date]")
-            .should("have.value", "2020-05-05")
-        cy.get("[data-cy=end-date]")
-            .should("have.value", "2020-05-15")
-    })
-
-    it("Check the crop dropdown contents", () => {
-        // Check first crop (index 0)
-        cy.get("[data-cy=crop-select]")
-            .children()
-            .eq(0)
-            .invoke('text')
-            .then(text => text.trim())
-            .should('eq', 'ARUGULA')
-        
-        // Check fifth crop (index 4)
-        cy.get("[data-cy=crop-select]")
-            .children()
-            .eq(4)
-            .invoke('text')
-            .then(text => text.trim())
-            .should('eq', 'BEAN-FAVA')
-        
-        // Check last crop
-        cy.get("[data-cy=crop-select]")
-            .children()
-            .then($children => {
-                const lastIndex = $children.length - 1
-                cy.get("[data-cy=crop-select]")
-                    .children()
-                    .eq(lastIndex)
-                    .invoke('text')
-                    .then(text => text.trim())
-                    .should('eq', 'ZUCCHINI')  // Note: We may need to verify this value too
-            })
+    it("Check the farm data", () => {
+        cy.get("[data-cy=generate-report-button]")
+            .click()
+            
+        // Approach 1: Testing full text content
+        cy.get("[data-cy=farm-name]")
+            .should("have.text", "Farm: Sample Farm")
+            
+        // Approach 2: Using contains for partial match
+        cy.get("[data-cy=farm-user]")
+            .should("contain.text", "manager1")
+            
+        // Approach 3: Using span with have.text for precise testing
+        cy.get("[data-cy=language-value]")
+            .should("have.text", "English")
     })
 })
--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.spec.js
@@ -13,5 +13,14 @@ describe("Test the harvest report generation", () => {
         cy.get("[data-cy=generate-report-button]")
             .should("exist")
             .click()
+
+         cy.get("[data-cy=report-header]").should("be.visible")
+    })
+
+    it("Check the farm data", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("contain.text", "Sample Farm")
+        cy.get("[data-cy=farm-user]").should("contain.text", "manager1")
+        cy.get("[data-cy=language]").should("have.text", "")
     })
 })
